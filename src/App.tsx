45d9acf7f75c37f9dd9721a14a04dd2e231f7582
Jan Ottenbreit <jan.ottenbreit@css.de>
2026-03-28 23:17:42 +0100

    Fix BrowserRouter basename trailing slash

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
         <AppNav />
         <Routes>
           <Route path="/" element={<Index />} />
